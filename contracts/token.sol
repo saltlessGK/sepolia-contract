52393146dc0053f1e03b0ec0d5469cf0d6de657f
SSETbbx <s4035894@rmit.edu.vn>
2026-03-28 02:58:34 +0700

    added in-line comments for contract and script

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; // OpenZeppelin module for ERC20 functionalities
+import "@openzeppelin/contracts/access/Ownable.sol"; // OpenZeppelin module for ownership control
 
 contract CustomToken is ERC20, Ownable {
-    string[] public data;
+    string[] public data; // Public array to store text metadata
+
+    // Constructor to set token identity and address of contract owner
     constructor() ERC20("CustomToken", "CSTK") Ownable(msg.sender) {}
+
+    // Function to mint new tokens to a certain address and append hexadecimal metadata to transaction data input
     function mint(address to, uint256 amount, bytes memory hexData) public onlyOwner {
         _mint(to, amount);
         data.push(string(hexData));
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,37 @@
 import { network } from "hardhat";
 import { parseUnits } from "ethers";
 
+// Connect to configured network and declared sender wallet (with private key in .env file)
 const { ethers } = await network.connect();
 
 async function main() {
-
+  // Retrieve the contract factory (properties) for CustomToken and deploy it
   const Token = await ethers.getContractFactory("CustomToken");
   const deploy = await Token.deploy();
   await deploy.waitForDeployment();
-  
+
+  // Output the deployment transaction hash
   const deployTx = deploy.deploymentTransaction();
   if (deployTx) {
     console.log("Contract deployment hash:", deployTx.hash);
   }
 
+  // Information on sended amounts, receiving address and metadata payload
   const recipient = "0xeE0633a08B3a0d4ff5170bAecab215359731524b";
   const amount = parseUnits("894", 18);
   const payload = "FINAL-4035894";
+
+  // Convert payload string to hexadecimal format
   const hexPayload = ethers.hexlify(
     ethers.toUtf8Bytes(payload)
   );
 
+  // Call mint() function from the deployed contract
   const tx = await deploy.mint(recipient, amount, hexPayload);
   console.log("Transaction hash:", tx.hash);
-
 }
 
+// Execute the main function with proper error handling
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
